fa8c2fdc9fe269cf0b5f2af50a8d55b517f279d6
pratikravindramane <p.mane7727@gmail.com>
2024-02-20 23:33:18 +0530

    very basic home page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,13 @@ function App() {
       <h3>
         Lets go for a <FaBeer />?
       </h3>
+      <div className="body">
+        <div className="navbar">NavBar</div>
+        <div className="hero">HeroSection</div>
+        <div className="about">AbouUs</div>
+        <div className="contact">ContactUs</div>
+        <div className="footer">Footer</div>
+      </div>
     </div>
   );
 }
