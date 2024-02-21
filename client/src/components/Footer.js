2cbde0c954d303232bc59975092356b7e4bd685c
pratikravindramane <p.mane7727@gmail.com>
2024-02-21 22:33:42 +0530

    footer started, last done

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">yello</div>
+            <div className="col-6">pollo</div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-3">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </footer>
+      <footer className="py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12 ">
+              <p className="text-center mb-0 text-white">
+                &copy;{new Date().getFullYear()} powered by Pratik Mane{" "}
+              </p>
+            </div>
+            {/* <div className="col-6">pollo</div> */}
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
