8f9b6c6ba52ed52e89d4c39db56fdc09ecad3020
pratikravindramane <p.mane7727@gmail.com>
2024-02-28 16:10:18 +0530

    footer done

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import newsLetter from "../images/newsletter.png";
+import { Link } from "react-router-dom";
+import { FaLinkedin, FaXTwitter, FaInstagram, FaGithub } from "react-icons/fa6";
 function Footer() {
   return (
     <>
@@ -33,19 +35,90 @@ function Footer() {
         <div className="container-xxl">
           <div className="row">
             <div className="col-4">
-              <h5 className="text-white mb-4">Contact Us</h5>
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div>
+                <address className="text-white fs-5">
+                  Ghatkopar west, mumbai 400086, maharashtra, india
+                </address>
+                <a
+                  href="tel:+91 8291735882"
+                  className="mt-3 d-block mb-2 text-white"
+                >
+                  +91 8291735882
+                </a>
+                <a
+                  href="mailto:pratik.mane7727@gmail.com"
+                  className="mt-2 d-block mb-2 text-white"
+                >
+                  pratik.mane7727@gmail.com
+                </a>
+                <div className="social-icons d-flex gap-30 align-items-center">
+                  <a
+                    href={"https://github.com/pratikravindramane/"}
+                    target="_blank"
+                    alt="social-icons"
+                    rel="noreferrer"
+                    className="text-white fs-4"
+                  >
+                    <FaGithub />
+                  </a>
+                  <a
+                    href={"https://linkedin.com/in/pratik-mane-22975019b/"}
+                    rel="noreferrer"
+                    target="_blank"
+                    alt="social-icons"
+                    className="text-white fs-4"
+                  >
+                    <FaLinkedin />
+                  </a>
+                  <a
+                    href={"https://twitter.com/pratikmane5598"}
+                    target="_blank"
+                    alt="social-icons"
+                    rel="noreferrer"
+                    className="text-white fs-4"
+                  >
+                    <FaXTwitter />
+                  </a>
+                  <a
+                    href={"https://www.instagram.com/mane.prem"}
+                    rel="noreferrer"
+                    target="_blank"
+                    alt="social-icons"
+                    className="text-white fs-4"
+                  >
+                    <FaInstagram />
+                  </a>
+                </div>
+              </div>
               <div></div>
             </div>
-            <div className="col-3">
-              <h5 className="text-white mb-4">Information</h5>
+            <div className="col-3 d-grid">
+              <h4 className="text-white mb-4">Information</h4>
+              <Link className="text-white py-2 mb-1">Privacy Policy</Link>
+              <Link className="text-white py-2 mb-1">Refund Policy</Link>
+              <Link className="text-white py-2 mb-1">Shipping Policy</Link>
+              <Link className="text-white py-2 mb-1">Terms of Services</Link>
+              <Link className="text-white py-2 mb-1">Blogs</Link>
               <div></div>
             </div>
-            <div className="col-3">
-              <h5 className="text-white mb-4">Account</h5>
+            <div className="col-3 d-grid">
+              <h4 className="text-white mb-4">Account</h4>
+              <Link className="text-white py-2 mb-1">Search</Link>
+              <Link className="text-white py-2 mb-1">About us</Link>
+              <Link className="text-white py-2 mb-1">FAQ</Link>
+              <Link className="text-white py-2 mb-1">Contact</Link>
               <div></div>
             </div>
-            <div className="col-2">
-              <h5 className="text-white mb-4">Quick Links</h5>
+            <div className="col-2 d-grid">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="footer-links d-grid flex-colomn">
+                <Link className="text-white py-2 mb-1">Accessories</Link>
+                <Link className="text-white py-2 mb-1">Laptops</Link>
+                <Link className="text-white py-2 mb-1">Headphones</Link>
+                <Link className="text-white py-2 mb-1">Tables</Link>
+                <Link className="text-white py-2 mb-1">Watch</Link>
+              </div>
               <div></div>
             </div>
           </div>
