6d2f7f494c4016f6753425f91e9a344f06db9d7a
pratikravindramane <p.mane7727@gmail.com>
2024-02-28 17:47:59 +0530

    home wrapper 2 done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,10 +5,15 @@ import catBanner from "../images/catbanner-01.jpg";
 import catBanner2 from "../images/catbanner-02.jpg";
 import catBanner3 from "../images/catbanner-03.jpg";
 import catBanner4 from "../images/catbanner-04.jpg";
+import service from "../images/service.png";
+import service2 from "../images/service-02.png";
+import service3 from "../images/service-03.png";
+import service4 from "../images/service-04.png";
+import service5 from "../images/service-05.png";
 function Home() {
   return (
     <>
-      <div className="home-wrapper-1 py-5">
+      <section className="home-wrapper-1 py-5">
         <div className="container-xxl">
           <div className="row">
             <div className="col-6">
@@ -81,7 +86,52 @@ function Home() {
             </div>
           </div>
         </div>
-      </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="services d-flex align-items-center justify-content-between">
+                <div className="d-flex gap-15 align-items-center">
+                  <img src={service} alt="services" />
+                  <div>
+                    <h6>Free shipping</h6>
+                    <p className="mb-0">From all order over $599</p>
+                  </div>
+                </div>
+                <div className="d-flex gap-15 align-items-center">
+                  <img src={service2} alt="services" />
+                  <div>
+                    <h6>Daily Suprise Offers</h6>
+                    <p className="mb-0">Save up to 25% off</p>
+                  </div>
+                </div>
+                <div className="d-flex gap-15 align-items-center">
+                  <img src={service3} alt="services" />
+                  <div>
+                    <h6>Support 24/7</h6>
+                    <p className="mb-0">Shop with an expert</p>
+                  </div>
+                </div>
+                <div className="d-flex gap-15 align-items-center">
+                  <img src={service4} alt="services" />
+                  <div>
+                    <h6>Affordable Prices</h6>
+                    <p className="mb-0">Get Factory Direct Price</p>
+                  </div>
+                </div>
+                <div className="d-flex gap-15 align-items-center">
+                  <img src={service5} alt="services" />
+                  <div>
+                    <h6>Secure Payments</h6>
+                    <p className="mb-0">100% Protected Payments</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
